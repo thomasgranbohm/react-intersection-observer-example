a0193d1eb62b2dc4a0ff5ffa3a29fb10f86d4a85
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-05-24 10:41:12 +0200

    Working example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+	const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const [items, setItems] = useState(original.slice());
+	const ref = useRef(undefined);
+
+	useEffect(() => {
+		console.log("Inside useEffect: %d\n%o", items.length, items);
+	}, [items]);
+
+	useEffect(() => {
+		const callback = ([entry]) => {
+			if (!entry.isIntersecting) return;
+
+			console.log("Inside callback: length: %d\n%o", items.length, items);
+			setItems([...items.slice(), ...original.slice()]);
+		};
+
+		const observer = new IntersectionObserver(callback, {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0,
+		});
+
+		if (ref.current) observer.observe(ref.current);
+
+		return () => {
+			if (ref.current) observer.disconnect();
+		};
+	}, [ref, items]);
+
+	return (
+		<div className="app">
+			<div className="list">
+				{items.map((item) => (
+					<div className="item">{item}</div>
+				))}
+				<div ref={ref}></div>
+			</div>
+		</div>
+	);
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
